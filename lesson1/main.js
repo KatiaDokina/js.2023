e9df65c955805e4a014bd745645172662539aa87
Pavlo1994Dokin <Katiazastavna@gmail.com>
2023-08-16 19:57:46 +0300

    add let string,number,true

---
+++ lesson1/main.js
@@ -0,0 +1,35 @@
+let s1 = 'hello';
+console.log(s1);
+let s2 = 'owu';
+console.log(s2);
+let s3 = 'com';
+console.log(s3);
+let s4 = 'ua';
+console.log(s4);
+let n1 = 1;
+console.log(n1)
+let n2 = 10;
+console.log(n2);
+let n3 = -999;
+console.log(n3);
+let n4 = 3.14;
+console.log(n4);
+let n5 = 2.7;
+console.log(n5);
+let n6 = 16;
+console.log(n6);
+let bRes = n2>n1;
+console.log(bRes);
+let bRes1 = n2<n1;
+console.log(bRes1);
+let firstName = 'Dokina'
+let middleName = 'Kateryna'
+let lastName = 'Ivanivna'
+let person = firstName +' ' + middleName +' ' +lastName;
+console.log(person);
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true
+console.log(c);
