44f94b351317ae3162b0c8473c1ca5cad85af9cf
Pavlo1994Dokin <Katiazastavna@gmail.com>
2023-08-19 03:35:17 +0300

    add homework2.html and main.js

---
+++ lesson2/main.js
@@ -0,0 +1,63 @@
+let colors = [
+    'black',
+    'white',
+    'blue',
+    'green',
+    'yellow',
+    'red',
+    'pink',
+    'purple',
+    'brown',
+    'orange'
+];
+console.log(colors);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
+console.log(colors[5]);
+console.log(colors[6]);
+console.log(colors[7]);
+console.log(colors[8]);
+console.log(colors[9]);
+
+let book1 = {
+    title: 'Atlas Shrugged',
+    pageCount: 391,
+    genre: 'novel'
+};
+let book2 = {
+    title: 'The Horse Dancer',
+    pageCount: 508,
+    genre: 'novel'
+};
+let book3 = {
+    title: 'Samotność w sieci',
+    pageCount: 420,
+    genre: 'novel'
+};
+console.log(book1);
+console.log(book2);
+console.log(book3);
+book1.authors = [{
+    name: 'Ayn Rand',
+    age: 77
+}];
+console.log(book1);
+book2.authors = [{
+    name: 'Pauline Sara Jo "Jojo" Moyes',
+    age: 54
+}];
+console.log(book2);
+book3.authors = [{
+    name: 'Janusz Leon Wiśniewski',
+    age: 69
+}];
+console.log(book3);
+let users = [
+    {name: 'diana', username: 'solod', password: 111},
+    {name: 'oleg', username: 'solod', password: 222},
+    {name: 'vova', username: 'solod', password: 333},
+
+]
